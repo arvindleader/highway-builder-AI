2cb7af01e7b3f2eb41156f467a369094d7217fe3
arvindleader <arvind.leader@gmail.com>
2025-10-06 22:14:20 +0530

    Delete frontend/src/src/api directory

--- frontend/src/src/api/estimate.js
+++
@@ -1,8 +0,0 @@
-export const getEstimate = async (data) => {
-  const res = await fetch('http://localhost:5000/api/estimate', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  });
-  return res.json();
-};
